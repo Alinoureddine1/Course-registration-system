71190c06a89936cede1c5e8049831b8d5f32e57d
Ali <66390544+Alinoureddine1@users.noreply.github.com>
2022-10-08 17:53:05 -0400

    Update CourseRegistration.cpp

--- CourseRegistration.cpp
+++ CourseRegistration.cpp
@@ -1,8 +1,7 @@
-
 #include "Course.h"
 #include "Student.h"
 #include "LectureTime.h"
-#include "CourseRegsitration.h"
+#include "CourseRegistration.h"
 #include <iostream>
 #include <string>
 using namespace std;
@@ -33,7 +32,7 @@ CourseRegistration::CourseRegistration(int maxS, int CurrentS)
 
 int CourseRegistration::getNumofS()
 {
-    return this->NumberOfCurrentStudents;
+    return (this->NumberOfCurrentStudents +1);
 }
 void CourseRegistration::setMaxS(int max)
 {
@@ -42,4 +41,55 @@ void CourseRegistration::setMaxS(int max)
 void CourseRegistration::setNumofS(int now)
 {
     this->NumberOfCurrentStudents = now;
+}
+
+void CourseRegistration::addStudent(Student * temp) 
+{
+    if (NumberOfCurrentStudents < MaxNumberOfStudents) {
+        this->sptr[NumberOfCurrentStudents] = temp;
+        NumberOfCurrentStudents++;
+    }
+    else {
+        cout << "Course is full\n" << endl;
+    }
+}
+Course& CourseRegistration::getcourse() {
+    return this->c1;
+}
+bool CourseRegistration::lookforstudent(Student* temp) 
+{
+    for (int i = 0; i < NumberOfCurrentStudents + 1; i++) {
+
+        if (this->sptr[i]->getid() == temp->getid() && this->sptr[i]->getname() == temp->getname()) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void CourseRegistration::displaycourse() {
+    this->c1.printcourse();
+    cout << "Enrollments: " << this->getNumofS() << " Seats available: " << this->getMaxS() - this->getNumofS() << endl;
+
+}
+
+void CourseRegistration::removestudent(Student* temp) {
+    for (int i = 0; i < NumberOfCurrentStudents; i++) {
+
+        if (sptr[i]->samestudent(temp)) {
+            delete sptr[i];
+            sptr[i] = nullptr;
+            
+        }
+    }
+}
+
+
+CourseRegistration::~CourseRegistration() 
+{
+    for (int i = 0; i < MaxNumberOfStudents; i++) {
+        delete sptr[i];
+    };
+    delete[] sptr;
+    cout << "Object of time CourseRegistration was deleted.\n" << endl;
 }
