13ee32fdf844d8aab2387268a23d907e97a0e967
Ali <66390544+Alinoureddine1@users.noreply.github.com>
2022-10-08 17:52:31 -0400

    Update dataManager.h

--- dataManager.h
+++ dataManager.h
@@ -1,1 +1,43 @@
 
+#pragma once
+#include "Course.h"
+#include "Student.h"
+#include "LectureTime.h"
+#include "CourseRegistration.h"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class dataManager {
+
+private:
+	CourseRegistration* courseArray[100]{nullptr};
+	Student* studentArray[2000]{ nullptr };
+	int positioncourse ;
+	int positionstd ;
+
+
+public:
+	dataManager();
+	bool insertCourseRegistration(CourseRegistration*);
+	bool insertStudent(Student*);
+	bool Delete(Student*);
+	bool registering_to_Course(Student*, Course&);
+	bool dropping_a_Course(Student*, Course&);
+	int courseEnrollment(Course&);
+	void studentCourses(int);
+	bool samestudent(Student*, Student*); //used to delete a certain student
+	bool studentinrecords(Student*);
+	void print();
+	~dataManager();
+
+
+
+
+
+
+
+
+};
